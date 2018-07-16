b7f07a104e9919b25d3208762cb7d0ab9eb7afda
Peter Salomonsen <pjsalomonsen@gmail.com>
2018-07-16 08:07:03 +0200

    Repeating drumtrack

--- drumtrack.js
+++ drumtrack.js
@@ -10,4 +10,46 @@ for(var n=0;n<output.getPortCount(); n++) {
 }
 output.openPort(outputIndex);
 
-setInterval(()=> output.sendMessage([0x91, 0x24, 127]), 600);
+const noteStringToNoteNumberMap = 
+    new Array(128).fill(null).map((v, ndx) => 
+        (['c','c#','d','d#','e','f','f#','g','g#','a','a#','b'])[ndx%12]+''+Math.floor(ndx/12)
+    ).reduce((prev, curr, ndx) => {
+        prev[curr] = ndx;
+        return prev;
+    }, {});
+
+const midimessages = [
+    [2, 'c3'],
+    [0, 'g#3'],
+    [4, 'e3'],
+    [0, 'g#3'],
+    [2, 'c3'],
+    [2, 'g#3'],
+    [2, 'c3'],
+    [2, 'e3'],
+    [0, 'g#3'],
+    [2, 'c3']
+].map(v => [v[0]*0.1, [0x91, noteStringToNoteNumberMap[v[1]], 127]]);
+
+let accumulatedDeltaTime = Date.now();
+
+let msgindex = 0;
+
+const playEvent = (msg) => {
+    if(msg) {
+        output.sendMessage(msg);
+    }
+
+    if(msgindex<midimessages.length) {
+        const evt = midimessages[msgindex++];
+        msgindex = msgindex % midimessages.length;
+
+        const deltatime = evt[0];
+        const nextmessage = evt[1];        
+        
+        accumulatedDeltaTime += deltatime * 1000;
+        setTimeout(() => playEvent(nextmessage), accumulatedDeltaTime - Date.now());
+    }     
+};
+
+playEvent();
