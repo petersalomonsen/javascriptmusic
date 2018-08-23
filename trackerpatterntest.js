e9363877dbb3573dcbea3708a7f2b532c2b7f672
Peter Salomonsen <pjsalomonsen@gmail.com>
2018-08-23 16:54:04 +0200

    some more melody

--- trackerpatterntest.js
+++ trackerpatterntest.js
@@ -27,38 +27,46 @@ const ch6 = new TrackerPattern(output, 5, 2);
         ]);
     }
         
-    ch6.play([
+    const littlepat = [
         [0, c5(2/3, 100), pitchbend(0, 0x2000, 1/2, 8)],
         [1/2, g5(1/4, 100)],
         [2/2, f5(1/4, 100)],
-        [3/2, g5(2/3, 100), pitchbend(0x1000, 0x2000, 1/2, 8)]
-        
-    ]);
-    
-    drumbase();
-    ch7.play([
-        [0, c4(), c5(), d5(), g5()]
-        
-    ]);
-    await waitForBeat(beat+=4);    
-    drumbase();
-    ch7.play([
-        [0, as3(), as4(), c5(), f5()]
-        
-    ]);
-    await waitForBeat(beat+=4);    
-    drumbase();
-    ch7.play([
-        [0, a3(), a4(), c5(), f5()]
-        
-    ]);
-    await waitForBeat(beat+=4);    
+        [3/2, g5(2/3, 100), pitchbend(0x1000, 0x2000, 1/2, 8)],
+        [4/2, as5(1/4, 100)],
+        [5/2, a5(1/4, 100)],
+        [6/2, f5(1/4, 100)],
+        [6.5/2, fs5(1/4, 60)],
+        [7/2, g5(1/4, 100)],
+    ];
 
-    drumbase();
-    ch7.play([
-        [0, c4(), g4(), c5(), e5()]
+    for(let lp = 0; lp < 10 ; lp++) {
+        ch6.play(littlepat);
         
-    ]);
-    await waitForBeat(beat+=4);    
+        drumbase();
+        ch7.play([
+            [0, c4(), c5(), d5(), g5()]
+            
+        ]);
+        await waitForBeat(beat+=4);    
+        drumbase();
+        ch7.play([
+            [0, as3(), as4(), c5(), f5()]
+            
+        ]);
+        await waitForBeat(beat+=4);    
+        drumbase();
+        ch6.play(littlepat);
+        ch7.play([
+            [0, a3(), a4(), c5(), f5()]
+            
+        ]);
+        await waitForBeat(beat+=4);    
 
+        drumbase();
+        ch7.play([
+            [0, c4(), g4(), c5(), e5()]
+            
+        ]);
+        await waitForBeat(beat+=4);    
+    }
 })();
