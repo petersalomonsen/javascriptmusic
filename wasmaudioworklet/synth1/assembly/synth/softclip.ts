d2d4a7469f5c4dc2d5a6f23b227bbfaf042d4cab
Peter Salomonsen <pjsalomonsen@gmail.com>
2020-02-01 14:20:09 +0100

    soft clipping function

---
+++ wasmaudioworklet/synth1/assembly/synth/softclip.ts
@@ -0,0 +1,15 @@
+// From https://ccrma.stanford.edu/~jos/pasp/Soft_Clipping.html#29695
+
+/**
+ * soft clip signal with 1/3 headroom as result
+ * @param signal signal to be clipped
+ */
+export function softclip(signal: f32): f32 {
+    if(signal > 1.0) {
+        return 2.0/3.0;
+    } else if(signal < -1.0) {
+        return -2.0/3.0;
+    } else {
+        return (signal - ((signal * signal * signal) / 3.0));
+    }
+}
