9e8fb806f2c6d06b2556a30dcffd9925a0af6fd7
Peter Salomonsen <pjsalomonsen@gmail.com>
2020-05-03 17:54:20 +0200

    fixes after yoshimi integration
    add build indicator to README
    fix recording for other tempos than 80 bpm
    include assemblyscript synth source bundle
    to be able to launch from github pages

--- wasmaudioworklet/midisequencer/editorfunctions.js
+++ wasmaudioworklet/midisequencer/editorfunctions.js
@@ -1,8 +1,9 @@
 import { getRecordedData } from '../webaudiomodules/wammanager.js';
 import { RecordConverter } from './recording.js';
 import { recordingStartTimeMillis } from './songcompiler.js';
+import { bpm } from './pattern.js';
 
 export async function insertMidiRecording(insertStringIntoEditor) {
-    insertStringIntoEditor(new RecordConverter(await getRecordedData(), 80,
+    insertStringIntoEditor(new RecordConverter(await getRecordedData(), bpm,
         recordingStartTimeMillis / 1000).trackerPatternData);
 }
