386e086c48473fecbe3df7e311fafb715ffe5dcd
Peter Salomonsen <pjsalomonsen@gmail.com>
2021-02-21 10:14:43 +0100

    switch to NEAR mainnet for auth
    add OfflineAudioContext without impl for avoiding crash in Safari
    increase size of camviewer
    v0.0.16

--- wasmaudioworklet/wasmgit/nearacl.js
+++ wasmaudioworklet/wasmgit/nearacl.js
@@ -2,16 +2,26 @@ import { loadScript } from '../common/scriptloader.js';
 
 
 // configure minimal network settings and key storage
-const nearconfig = {
+const nearconfig_testnet = {
     nodeUrl: 'https://rpc.testnet.near.org',
     walletUrl: 'https://wallet.testnet.near.org',
     helperUrl: 'https://helper.testnet.near.org',
     contractName: 'acl.testnet',
     deps: {
-      keyStore: null
+        keyStore: null
     }
 };
-  
+
+const nearconfig = {
+    nodeUrl: 'https://rpc.mainnet.near.org',
+    walletUrl: 'https://wallet.near.org',
+    helperUrl: 'https://helper.mainnet.near.org',
+    contractName: 'wasmgit.near',
+    deps: {
+        keyStore: null
+    }
+};
+
 export let authdata = null;
 
 // open a connection to the NEAR platform
@@ -32,12 +42,12 @@ async function loadAccountData() {
     let currentUser = {
         accountId: walletConnection.getAccountId()
     }
-    
-    const tokenMessage = btoa(JSON.stringify({accountId: currentUser.accountId, iat: new Date().getTime()}));
+
+    const tokenMessage = btoa(JSON.stringify({ accountId: currentUser.accountId, iat: new Date().getTime() }));
     const signature = await walletConnection.account()
         .connection.signer
-            .signMessage(new TextEncoder().encode(tokenMessage), currentUser.accountId
-    );
+        .signMessage(new TextEncoder().encode(tokenMessage), currentUser.accountId
+        );
 
     authdata = {
         accessToken: tokenMessage + '.' + btoa(String.fromCharCode(...signature.signature)),
@@ -48,7 +58,7 @@ async function loadAccountData() {
 
 export async function initNear() {
     await loadScript('https://cdn.jsdelivr.net/gh/nearprotocol/near-api-js/dist/near-api-js.js');
-    
+
     nearconfig.deps.keyStore = new nearApi.keyStores.BrowserLocalStorageKeyStore();
     window.near = await nearApi.connect(nearconfig);
     const walletConnection = new nearApi.WalletConnection(near);
