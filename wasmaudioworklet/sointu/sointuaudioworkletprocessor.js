f95ae495cc265285a3662bd293d112b5ccb0eb05
Peter Salomonsen <pjsalomonsen@gmail.com>
2023-03-19 21:25:57 +0100

    no need for js imports

--- wasmaudioworklet/sointu/sointuaudioworkletprocessor.js
+++ wasmaudioworklet/sointu/sointuaudioworkletprocessor.js
@@ -10,12 +10,7 @@ class SointuAudioWorkletProcessor extends AudioWorkletProcessor {
                 if (this.wasmInstance && msg.data.livewasmreplace) {
                     tick = this.wasmInstance.getTick();
                 }
-                this.wasmInstance = (await WebAssembly.instantiate(msg.data.wasm, {
-                    m: {
-                        pow: Math.pow,
-                        log2: Math.log2,
-                        sin: Math.sin
-                    }
+                this.wasmInstance = (await WebAssembly.instantiate(msg.data.wasm, {                   
                 })).instance.exports;
 
                 this.samplebuf = new Float32Array(this.wasmInstance.m.buffer,
@@ -39,10 +34,10 @@ class SointuAudioWorkletProcessor extends AudioWorkletProcessor {
             this.wasmInstance.render_128_samples();
             
             let bufpos = this.samplepos * 2;
+
             for (let i = 0; i < SAMPLE_FRAMES && this.samplepos < this.samplebuf.length; i++) {                
                 output[0][i] = this.samplebuf[bufpos++];
-                output[1][i] = this.samplebuf[bufpos++];
-                
+                output[1][i] = this.samplebuf[bufpos++];                
             }
             this.samplepos += SAMPLE_FRAMES;
         }
