361521d8670a87eebd016940b968365916f13561
Peter Salomonsen <pjsalomonsen@gmail.com>
2024-03-30 14:39:04 +0100

    variable number of samples to fill (#103)
    Add a new function fillSampleBufferWithNumSamples to exported wasm, so that it is possible to support building plugins with variable block sizes.
    
    v0.0.42

--- wasmaudioworklet/synth1/browsercompilerwebworker.js
+++ wasmaudioworklet/synth1/browsercompilerwebworker.js
@@ -28,7 +28,7 @@ function createWebAssemblySongData(song, mode = EXPORT_MODE_MIDISYNTH_MULTIPART_
             export const midipartschedule: MidiSequencerPartSchedule[] = [new MidiSequencerPartSchedule(0, 0)];
         `;
         assemblyscriptsynthsources[wasi_main_src] = `
-            export { fillSampleBuffer, samplebuffer, allNotesOff, shortmessage, getActiveVoicesStatusSnapshot } from './midi/midisynth';
+            export { fillSampleBuffer, fillSampleBufferWithNumSamples, samplebuffer, allNotesOff, shortmessage, getActiveVoicesStatusSnapshot } from './midi/midisynth';
             export { seek, playEventsAndFillSampleBuffer, currentTimeMillis } from './midi/sequencer/midisequencer';
             import { midipartschedule } from './midi/sequencer/midiparts';
 
